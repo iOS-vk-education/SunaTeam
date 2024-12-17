a5c4aa3ba1fe02c53c248bf945d644f0913a0527
Mikhail <zimanov.mm@mail.ru>
2024-12-17 16:05:03 +0300

    Remake profile screens to MVVM arch

---
+++ SunaTravel/Profile/Models/ProfileData.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileData.swift
+//  Suna Travel
+//
+//  Created by salfetka on 20.11.2024.
+//
+
+import Foundation
+
+struct ProfileData {
+    var name: String
+    var email: String
+    var location: String
+    var phoneNumber: String
+    var rewardPoints: String
+    var travelTips: String
+    var bucketList: String
+}
