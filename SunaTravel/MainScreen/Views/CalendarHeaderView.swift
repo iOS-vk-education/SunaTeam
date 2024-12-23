bb631b7e97ccce332bcbdd8895108c98e6a58f32
vanechka <tivan08122004@gmail.com>
2024-12-23 16:24:57 +0300

    conflicts

---
+++ SunaTravel/MainScreen/Views/CalendarHeaderView.swift
@@ -0,0 +1,74 @@
+//
+//  CalendarHeaderView.swift
+//  SunaTravel
+//
+//  Created by Иван Тарасюк on 14.12.2024.
+//
+
+import SwiftUI
+
+fileprivate struct UIConstants {
+    //static let daysInWeek = 7
+    static let weekdayAdjustmentForSunday: Int = 6
+    static let weekdayAdjustment: Int = 2
+    static let circleSize: CGFloat = 40
+    static let horizontalSpacing: CGFloat = 15
+    static let topPadding: CGFloat = 20
+}
+
+struct CalendarHeaderView: View {
+    let currentDate = Date()
+    let calendar = Calendar.current
+    let weekdaySymbols = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    var daysInWeek: Int {
+        weekdaySymbols.count
+    }
+    
+
+    var startOfWeek: Date {
+        let weekday = calendar.component(.weekday, from: currentDate)
+        let daysToSubtract = (weekday == 1) ? UIConstants.weekdayAdjustmentForSunday : weekday - UIConstants.weekdayAdjustment
+        return calendar.date(byAdding: .day, value: -daysToSubtract, to: currentDate) ?? Date()
+    }
+    
+    var weekDates: [Date] {
+        return (0..<daysInWeek).map { calendar.date(byAdding: .day, value: $0, to: startOfWeek)! }
+    }
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: UIConstants.horizontalSpacing) {
+                ForEach(0..<daysInWeek, id: \.self) { index in
+                    let date = weekDates[index]
+                    let dayOfMonth = calendar.component(.day, from: date)
+                    let weekdaySymbol = weekdaySymbols[index]
+                    
+                    let isToday = calendar.isDate(date, inSameDayAs: currentDate)
+                    
+                    VStack {
+                        Text(weekdaySymbol)
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                        Text("\(dayOfMonth)")
+                            .font(.title3.bold())
+                            .foregroundColor(isToday ? Color.orange : Color.black)
+                            .frame(width: UIConstants.circleSize, height: UIConstants.circleSize)
+                            .background(isToday ? Color.orange.opacity(0.2) : Color.clear)
+                            .clipShape(Circle())
+                    }
+                }
+            }
+        }
+        .padding(.horizontal)
+        .padding(.top, UIConstants.topPadding)
+    }
+}
+
+struct CalendarHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarHeaderView()
+            .previewLayout(.sizeThatFits)
+    }
+}
+
---
+++ SunaTravel/MainScreen/Views/MainViewController.swift
@@ -0,0 +1,65 @@
+//
+//  MainViewController.swift
+//  SunaTravel
+//
+//  Created by Иван Тарасюк on 14.12.2024.
+//
+
+import SwiftUI
+
+fileprivate struct UIConstants {
+    static let VStackSpacing: CGFloat = 20
+}
+
+struct MainView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                CalendarHeaderView()
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("My Schedule")
+                            .font(.title3.bold())
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                    
+                    ScrollView {
+                        LazyVStack(spacing: UIConstants.VStackSpacing) {
+                            ForEach(scheduleItems) { item in
+                                ScheduleCell(item: item)
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                .padding(.top)
+                NavigationBar()
+            }
+            .navigationBarTitle("Schedule", displayMode: .inline)
+            .navigationBarItems(
+                leading: Button(action: {
+                    // MARK: - действие для кнопки назад
+                }) {
+                    Image(systemName: "arrow.backward")
+                        .foregroundColor(.black)
+                },
+                trailing: Button(action: {
+                    // MARK: - действие для перехода в профиль
+                }) {
+                    Image(systemName: "person.crop.circle")
+                        .foregroundColor(.black)
+                }
+                
+            )
+        }
+        .background(Color(.systemGray6))
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
