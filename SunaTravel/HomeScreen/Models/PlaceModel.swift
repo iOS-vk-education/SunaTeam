ba69429224a1a531cfb5cc4deaaaf1135d4effc5
Mikhail <zimanov.mm@mail.ru>
2024-12-24 02:19:47 +0300

    Resolve conflicts

---
+++ SunaTravel/HomeScreen/Models/PlaceModel.swift
@@ -0,0 +1,14 @@
+//
+//  PlaceModel.swift
+//  SunaTravel
+//
+//  Created by salfetka on 21.12.2024.
+//
+
+import Foundation
+
+struct Place {
+    let title: String
+    let subtitle: String
+    let imageName: String
+}
